d163ab21da05d5d17bff409fe382b6d125b29e87
siumaii <andysiutang@gmail.com>
2020-05-23 00:35:50 +0200

    first commit

---
+++ intro-to-node/index.js
@@ -0,0 +1,7 @@
+// const fs = require('fs');
+// fs.copyFileSync("file1.txt", "file2.txt");
+var superheroes = require('superheroes');
+
+var mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
